227020dd202625ab5a67e168fdf75a9a7b3f508f
sunliang <sunliang711@163.com>
2017-07-23 17:28:08 +0800

    add file samba

--- tools/nginxLogRotate.sh
+++ tools/nginxLogRotate.sh
@@ -12,19 +12,6 @@ echo "pidfile: $pidfile"
 pid=$(cat $pidfile)
 echo "pid: $pid"
 
-# slientExe(){
-#     eval "$*" >/dev/null 2>&1
-# }
-
-# slientExe rm -f "${logfile}.7"
-# slientExe mv "${logfile}.6" "${logfile}.7"
-# slientExe mv "${logfile}.5" "${logfile}.6"
-# slientExe mv "${logfile}.4" "${logfile}.5"
-# slientExe mv "${logfile}.3" "${logfile}.4"
-# slientExe mv "${logfile}.2" "${logfile}.3"
-# slientExe mv "${logfile}.1" "${logfile}.2"
-# slientExe mv "${logfile}" "${logfile}.1"
-
 
 #rotate backup
 bk(){
