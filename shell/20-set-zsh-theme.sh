d7c492d660817c07a664882946fd4c06d5243a99
sunliang <sunliang711@163.com>
2017-07-29 00:29:43 +0800

    ~$user not work,so set HOME to $(eval echo ~$user)

--- shell/20-set-zsh-theme.sh
+++ shell/20-set-zsh-theme.sh
@@ -3,18 +3,17 @@ SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPTDIR"
 
 user=${SUDO_USER:-$(whoami)}
-if [ ! -d ~$user/.oh-my-zsh ];then
+HOME=$(eval echo ~$user)
+if [ ! -d $HOME/.oh-my-zsh ];then
     echo -e "Cann't find oh-my-zsh direcotry!$(tput setaf 1)\u2717"
     exit 1
 fi
-cp *.zsh-theme ~$user/.oh-my-zsh/themes
-sed -ibak 's/\(ZSH_THEME=\).\{1,\}/\1"zeta"/' ~$user/.zshrc
+cp *.zsh-theme $HOME/.oh-my-zsh/themes
+sed -ibak 's/\(ZSH_THEME=\).\{1,\}/\1"zeta"/' $HOME/.zshrc
 rm ~$user/.zshrcbak
 #install zsh-syntax-highlighting
-cd ~$user
-if [[ ! -d .zsh-syntax-highlighting ]];then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .zsh-syntax-highlighting
+if [[ ! -d $HOME/.zsh-syntax-highlighting ]];then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh-syntax-highlighting
 fi
-echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~$user/.zshrc
-cd - >/dev/null 2>&1
+echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
 
