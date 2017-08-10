4497e2e2c897b1863c02b0754da59810e27b2112
sunliang <sunliang711@163.com>
2017-08-10 21:19:38 +0800

    月底的时候,先重启,重启的时候在关闭的时候会把记录的流量存到数据库中 然后可以保存记录的流量到其他数据表或文件中(TODO) 最后清空所有流量(因为到月底了)

--- service/iptablesService/iptables-cron-lastDay.sh
+++ service/iptablesService/iptables-cron-lastDay.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 
+
+#restart iptables service
+systemctl restart iptables
+
 #save all traffic of this month to db
 #TODO
-
 
 #clear all traffic
 rulesManager.sh clearAll
 
-#restart iptables service
-systemctl restart iptables
