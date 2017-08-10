c6c1a98f49331bcc7ffb147c8ce9aef6ecf6f2fd
sunliang <sunliang711@163.com>
2017-08-10 22:33:34 +0800

    save traffic

--- service/iptablesService/iptables-cron-lastDay.sh
+++ service/iptablesService/iptables-cron-lastDay.sh
@@ -5,8 +5,12 @@ export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 #restart iptables service
 systemctl restart iptables
 
-#save all traffic of this month to db
-#TODO
+#save all traffic of this month
+month=$(date +%Y/%m)
+traffic=$(rulesManager list)
+echo "$month" >>ROOT/traffic-month
+echo "$traffic" >>ROOT/traffic-month
+
 
 #clear all traffic
 rulesManager.sh clearAll
