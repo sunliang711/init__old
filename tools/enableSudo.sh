acd4ec73106971573ff274baeeab2dab1c9a207c
sunliang <sunliang711@163.com>
2019-07-02 13:07:49 +0800

    enableSudo.sh

--- tools/enableSudo.sh
+++ tools/enableSudo.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 if (($EUID!=0));then
-    echo "Run as root."
+    echo "MUST run as root."
     exit 1
 fi
-NOPASS=nopass
-if ! grep -qE '^#includedir /etc/sudoers.d$' /etc/sudoers;then
-    echo '#includedir /etc/sudoers.d' >> /etc/sudoers
-fi
 
-if ! grep -qE "^$NOPASS:" /etc/group;then
-    groupadd $NOPASS
-fi
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+reset=$(tput sgr0)
 
-cat<<EOF>/etc/sudoers.d/custom-sudoers
-%$NOPASS ALL=(ALL:ALL) NOPASSWD:ALL
-EOF
-
-user=${SUDO_USER:-$(whoami)}
-echo -n "add \"$user\" to group \"$NOPASS\" to permit no password permission?[y/N] "
-read xx
-if [[ $xx =~ [yY] ]];then
-    usermod -aG $NOPASS $user
-else
-    echo -n "add who to group \"$NOPASS\"? (empty to quit) "
-    read yy
-    if [ -n "$yy" ];then
-        if ! grep -qE "^$yy:" /etc/passwd;then
-            echo "user: \"$yy\" not exist."
+user=${1}
+if [ -z "$user" ];then
+    if [ -n "${SUDO_USER}" ];then
+        user="${SUDO_USER}"
+        echo -n "Enable user:$user ?[y/n] "
+        read xx
+        if [ $xx = "n" ];then
+            echo "${red}Exit${reset}."
             exit 1
         fi
-        usermod -aG $NOPASS $yy
     fi
 fi
+
+echo "Enable user:${green}$user${reset} to sudo nopass permission..."
+customRule=/etc/sudoers.d/nopass
+cat<<EOF>>"$customRule"
+$user ALL=(ALL:ALL) NOPASSWD:ALL
+EOF
+
+sort "$customRule" | uniq > "${customRule}.tmp" && mv "${customRule}.tmp" "${customRule}"
+
+echo "${green}Done.${reset}"
