f37966266eeb845b945d82662bbfd373aaec9fe9
sunliang <sunliang711@163.com>
2019-08-23 17:20:23 +0800

    ln -sf instead of cp

--- shell/20-set-zsh-theme.sh
+++ shell/20-set-zsh-theme.sh
@@ -18,7 +18,8 @@ if [ ! -d $HOME/.oh-my-zsh ];then
     echo -e "Cann't find oh-my-zsh direcotry!$(tput setaf 1)\u2717"
     exit 1
 fi
-cp *.zsh-theme $HOME/.oh-my-zsh/themes
+# cp *.zsh-theme $HOME/.oh-my-zsh/themes
+ln -sf $(PWD)/*.zsh-theme $HOME/.oh-my-zsh/themes
 sed -ibak 's/\(ZSH_THEME=\).\{1,\}/\1"zeta"/' $HOME/.zshrc
 rm $HOME/.zshrcbak
 #install zsh-syntax-highlighting
