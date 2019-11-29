6b9aa6122044f7ba7bdf50174188ab02b026ba27
sunliang <sunliang711@163.com>
2019-11-29 20:19:51 +0800

    install docker on debian9

---
+++ tools/docker/docker-debian9.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+root=0
+
+check(){
+    if [ "${EUID}" -ne "$root" ];then
+        echo "Need root priviledge."
+        exit 0
+    fi
+}
+
+check
+
+install(){
+    apt update
+    apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    apt update
+    apt install docker-ce
+}
+
+install
