edd018c10da97bcd69f0ea04a5a1b456f4beaf7e
sunliang <sunliang711@163.com>
2019-11-29 20:22:19 +0800

    chmod +x

--- tools/docker/docker-debian9.sh
+++ tools/docker/docker-debian9.sh
@@ -14,11 +14,11 @@ check
 
 install(){
     apt update
-    apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
     apt update
-    apt install docker-ce
+    apt install docker-ce -y
 }
 
 install
