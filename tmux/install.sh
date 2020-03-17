e0f0b3b2427f951dfb6917316156b6b7f9f62b2c
sunliang <sunliang711@163.com>
2020-03-17 14:24:50 +0800

    vim plugin msg;tmux setting;

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,9 +8,34 @@ home=$(eval echo ~$user)
 
 
 cat<<EOF>$home/.tmux.conf
-set -g display-panes-time 3000 #3s
-#set-window-option mode-keys vi
+set-window-option -g mode-keys vi
+set -g display-panes-time 10000 #10s
+
+bind | split-window -h -c "#{pane_current_path}"
+bind - split-window -v -c "#{pane_current_path}"
+
 set -g mouse on
+
+# not work
+# bind-key -T vi-copy 'v' begin-selection
+# bind-key -T vi-copy 'y' copy-selection
+
+# works well
+bind-key -Tcopy-mode-vi 'v' send -X begin-selection
+bind-key -Tcopy-mode-vi 'y' send -X copy-selection
+
+bind k select-pane -U
+bind j select-pane -D
+bind h select-pane -L
+bind l select-pane -R
+
+bind H resize-pane -L 4
+bind L resize-pane -R 4
+bind J resize-pane -D 4
+bind K resize-pane -U 4
+
+bind s source-file ~/.tmux.conf
+
 EOF
 
 
