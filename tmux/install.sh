e8a5e7518794441a6101c4b0fa103e77d0ea04c1
sunliang <sunliang711@163.com>
2020-03-18 12:43:37 +0800

    add tmux setting

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,34 +8,63 @@ home=$(eval echo ~$user)
 
 
 cat<<EOF>$home/.tmux.conf
+##################################################
+# enable vi mode
 set-window-option -g mode-keys vi
 set -g display-panes-time 10000 #10s
 
+##################################################
+# set croll history limit
+set -g history-limit 8000
+
+##################################################
+# split window
 bind | split-window -h -c "#{pane_current_path}"
 bind - split-window -v -c "#{pane_current_path}"
 
+##################################################
+# enable mouse
 set -g mouse on
 
+##################################################
+# vi mode copy
+# works well
+bind-key -Tcopy-mode-vi 'v' send -X begin-selection
+bind-key -Tcopy-mode-vi 'y' send -X copy-selection
 # not work
 # bind-key -T vi-copy 'v' begin-selection
 # bind-key -T vi-copy 'y' copy-selection
 
-# works well
-bind-key -Tcopy-mode-vi 'v' send -X begin-selection
-bind-key -Tcopy-mode-vi 'y' send -X copy-selection
-
+##################################################
+# select pane
 bind k select-pane -U
 bind j select-pane -D
 bind h select-pane -L
 bind l select-pane -R
 
+##################################################
+# resize pane
 bind H resize-pane -L 4
 bind L resize-pane -R 4
 bind J resize-pane -D 4
 bind K resize-pane -U 4
 
-bind r source-file ~/.tmux.conf
+##################################################
+# edit .tmux.conf
+bind e new-window -n '~/.tmux.conf' "sh -c 'vim ~/.tmux.conf && tmux source ~/.tmux.conf'"
+
+##################################################
+# search text in current pane
+bind-key / copy-mode \; send-key ?
+
+##################################################
+# reload config file
+bind r source-file ~/.tmux.conf \; display "Reloaded tmux config!"
+
+##################################################
+# show options
+bind o show-options -g
+
 
 EOF
-
 
