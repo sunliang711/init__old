e45c8cbf3067a60983ca46c1655e94bec2bd6939
sunliang <sunliang711@163.com>
2020-04-01 21:26:59 +0800

    add installFzf.sh

---
+++ tools/installFzf.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+rpath="$(readlink ${BASH_SOURCE})"
+if [ -z "$rpath" ];then
+    rpath=${BASH_SOURCE}
+fi
+thisDir="$(cd $(dirname $rpath) && pwd)"
+cd "$thisDir"
+
+user="${SUDO_USER:-$(whoami)}"
+home="$(eval echo ~$user)"
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+cyan=$(tput setaf 5)
+        bold=$(tput bold)
+reset=$(tput sgr0)
+runAsRoot(){
+    verbose=0
+    while getopts ":v" opt;do
+        case "$opt" in
+            v)
+                verbose=1
+                ;;
+            \?)
+                echo "Unknown option: \"$OPTARG\""
+                exit 1
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
+    cmd="$@"
+    if [ -z "$cmd" ];then
+        echo "${red}Need cmd${reset}"
+        exit 1
+    fi
+
+    if [ "$verbose" -eq 1 ];then
+        echo "run cmd:\"${red}$cmd${reset}\" as root."
+    fi
+
+    if (($EUID==0));then
+        sh -c "$cmd"
+    else
+        if ! command -v sudo >/dev/null 2>&1;then
+            echo "Need sudo cmd"
+            exit 1
+        fi
+        sudo sh -c "$cmd"
+    fi
+}
+###############################################################################
+        # write your code below (just define function[s])
+###############################################################################
+# TODO
+install(){
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
+
+
+
+###############################################################################
+# write your code above
+###############################################################################
+help(){
+    cat<<EOF2
+Usage: $(basename $0) ${bold}CMD${reset}
+
+${bold}CMD${reset}:
+EOF2
+    perl -lne 'print "\t$2" if /^(function)?\s*?(\w+)\(\)\{$/' $(basename ${BASH_SOURCE})
+}
+
+case "$1" in
+     ""|-h|--help|help)
+        help
+        ;;
+    *)
+        "$@"
+esac
